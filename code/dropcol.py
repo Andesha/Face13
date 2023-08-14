8e35cbcb0da7627b875c810f45219e2a18ec014f
Tyler Collins <tk11br@sharcnet.ca>
2023-08-14 14:47:16 -0400

    drop HED column from events and just use root ref

--- code/dropcol.py
+++ code/dropcol.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sys
 
 df = pd.read_csv(sys.argv[1], sep='\t')
-df = df.drop(['sample', 'response_time', 'trial_type'],axis=1)
+# df = df.drop(['sample', 'response_time', 'trial_type'],axis=1)
+df = df.drop(['HED'],axis=1)
 df.to_csv(sys.argv[1], sep='\t', index=None)
 print('Done')
